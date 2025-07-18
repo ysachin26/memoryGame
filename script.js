f989a93487f042192bb12ac09de83b3d82f04e6e
sachin <tocontactsy@gmail.com>
2025-07-18 19:06:16 +0530

    added dynamic layout for game modes to resize card's size

--- script.js
+++ script.js
@@ -13,209 +13,218 @@ let CardFaces = [
   "surprised-face",
   "tears-of-joy-face",
   "wink-face",
-  "worried-face"
+  "worried-face",
 ];
 
-let count ;
-selectedMode("easy")
+let count;
 
-  let matchCount = 0;
-  let moveCount = 0;
-  let bestScore = 0;
-  let isSoundOn = true;  
-
-
- document.querySelector(".match-count").innerHTML = matchCount;
- document.querySelector(".move-count").innerHTML =  moveCount;
- const winningBoard = document.querySelector(".winningpopup")
- 
-//NOTE:this shuffle method is not efficient but it good for small games like this.
-//future upgrade of this is fisher yate algorithm (best and optimised method)
-
-
-const flipSound = new Audio("sound-effects/card-flip-sound.mp3");
-const successSound = new Audio("sound-effects/success.mp3");
-const victorySound = new Audio("sound-effects/Victory Sound Effect.mp3");
- 
-
-const mainGameSound = new Audio("sound-effects/main-game-sound.mp3");
-mainGameSound.volume = 0.6;
-mainGameSound.loop = true;
- mainGameSound.play();
-document.addEventListener(
-  "click",
-  () => {
-    mainGameSound.play();
-  },
-  { once: true }
-);
+let matchCount = 0;
+let moveCount = 0;
+let bestScore = 0;
+let isSoundOn = true;
 
 let firstCard = null;
 let secondCard = null;
 let lockedCard = false;
 
+selectedMode("easy");
+document.querySelector(".match-count").innerHTML = matchCount;
+document.querySelector(".move-count").innerHTML = moveCount;
+const winningBoard = document.querySelector(".winningpopup");
 
-function selectedMode(mode)
-{
-  if(mode==="easy")
-  {
-    count = 6;
-    renderGameBoard(count)
+//NOTE:this shuffle method is not efficient but it good for small games like this.
+//future upgrade of this is fisher yate algorithm (best and optimised method)
+
+const flipSound = new Audio("sound-effects/card-flip-sound.mp3");
+const successSound = new Audio("sound-effects/success.mp3");
+const victorySound = new Audio("sound-effects/Victory Sound Effect.mp3");
+
+const mainGameSound = new Audio("sound-effects/main-game-sound.mp3");
+mainGameSound.volume = 0.6;
+mainGameSound.loop = true;
+mainGameSound.play();
+document.addEventListener(
+  "click",
+  () => {
+    mainGameSound.play();
+  },
+  { once: true },
+);
+
+function setGridLayout(mode) {
+  const gameBoard = document.querySelector(".gameboard");
+  const cards = document.querySelectorAll(".card");
+
+  // Set number of columns based on difficulty
+  if (mode === "easy" || mode === "medium") {
+    gameBoard.style.gridTemplateColumns = "repeat(6, 1fr)";
   }
-  else if (mode ==="medium")
-  {
-    count = 9;
-        renderGameBoard(count)
-  }
-  else if (mode ==="hard")
-  {
-   count = 12;
-       renderGameBoard(count)
-  }
-  else
-  {
-    count = 6;
-        renderGameBoard(count)
-  }
-  return count
+  requestAnimationFrame(() => {
+    cards.forEach((card) => {
+      if (mode === "hard") {
+        card.style.width = "130px";
+        card.style.height = "150px";
+      }
+    });
+  });
 }
 
+function selectedMode(mode) {
+  if (mode === "easy") {
+    count = 6;
+  } else if (mode === "medium") {
+    count = 9;
+  } else if (mode === "hard") {
+    count = 12;
+  } else {
+    count = 6;
+  }
+  renderGameBoard(count);
+  setGridLayout(mode);
+  return count;
+}
 
-function renderGameBoard(count)
-{
-
+function renderGameBoard(count) {
   const GameBoard = document.querySelector(".gameboard");
 
-   GameBoard.innerHTML = "";
+  GameBoard.innerHTML = "";
 
- let selectedCart = [...CardFaces].slice(0, count).sort(() => 0.5 - Math.random());
+  let selectedCart = [...CardFaces]
+    .slice(0, count)
+    .sort(() => 0.5 - Math.random());
 
-let cardPairs = [...selectedCart, ...selectedCart];
+  let cardPairs = [...selectedCart, ...selectedCart];
 
-cardPairs.sort(() => 0.5 - Math.random()); // You can shuffle again here
+  cardPairs.sort(() => 0.5 - Math.random()); // You can shuffle again here
 
- cardPairs.forEach((cardName) => {
+  cardPairs.forEach((cardName) => {
+    //creating an html elememt
+    const card = document.createElement("div");
+    card.classList.add("card");
 
-  //creating an html elememt
-  const card = document.createElement("div");
-  card.classList.add("card");
+    const front = document.createElement("img");
+    front.classList.add("front");
+    front.src = `Images/front-face/${cardName}.png`;
 
-  const front = document.createElement("img");
-  front.classList.add("front");
-  front.src = `Images/front-face/${cardName}.png`;
+    const back = document.createElement("img");
+    back.classList.add("back");
+    back.src = `Images/back-face/back-card.png`;
 
-  const back = document.createElement("img");
-  back.classList.add("back");
-  back.src = `Images/back-face/back-card.png`;
+    card.appendChild(front);
+    card.appendChild(back);
+    GameBoard.appendChild(card);
 
-  card.appendChild(front);
-  card.appendChild(back);
-  GameBoard.appendChild(card);
-
-  card.addEventListener("click", () => {
-    moveCount++;
-    document.querySelector(".move-count").innerHTML = Math.floor(moveCount/2);
-    if (lockedCard) return;
-    if (card.classList.contains("flipped")) return;
-    flipSound.currentTime = 0;
-    flipSound.volume = 0.6;
-    flipSound.play();
-    card.classList.add("flipped");
-
-    //first click if card is null - then storing card into first card and return
-    if (!firstCard) {
-      firstCard = card;
-      return;
-    }
-    //when 2nd click event is triggered the upper if logic will be skipped and 2nd will get
-    //locked card marked as true so another click will not applicable
-    secondCard = card;
-    lockedCard = true;
-
-     
-    const firstImg = firstCard.querySelector(".front").src;
-    const secondImg = secondCard.querySelector(".front").src;
-    //matching the urls of both card
-  
-    if (firstImg === secondImg) {
-     matchCount++
-        document.querySelector(".match-count").innerHTML = matchCount;
-      successSound.currentTime = 0;
-      successSound.play();
-      //in case of card match - flip is not done and cards fill remain flipped to its front face
-      firstCard = null;
-      secondCard = null;
-      lockedCard = false;
-
-      const allCards = document.querySelectorAll(".card");
-      const allflipped = [...allCards].every((card) =>
-        card.classList.contains("flipped")
+    card.addEventListener("click", () => {
+      moveCount++;
+      document.querySelector(".move-count").innerHTML = Math.floor(
+        moveCount / 2,
       );
+      if (lockedCard) return;
+      if (card.classList.contains("flipped")) return;
+      flipSound.currentTime = 0;
+      flipSound.volume = 0.6;
+      flipSound.play();
+      card.classList.add("flipped");
 
-      if (allflipped) {
-        mainGameSound.pause();
-        victorySound.volume = 0.8;
-        victorySound.play();
-         recordGameTime();
-        setTimeout(() => {
-          winningBoard.classList.remove("winningpopup-hidden")
-        }, 300);
-   document.querySelector(".gameScore").innerHTML = "Your Score: " + calculateScore(saveMode || "easy", moveCount, matchCount, minutes, seconds);
-  
-      let newScore =  calculateScore(saveMode || "easy", moveCount, matchCount, minutes, seconds);
-      if(newScore>bestScore)
-      {
-         bestScore = newScore
-          document.querySelector(".score-count").innerHTML= bestScore;
+      //first click if card is null - then storing card into first card and return
+      if (!firstCard) {
+        firstCard = card;
+        return;
       }
+      //when 2nd click event is triggered the upper if logic will be skipped and 2nd will get
+      //locked card marked as true so another click will not applicable
+      secondCard = card;
+      lockedCard = true;
 
-     
-      }
-    } else {
-      setTimeout(() => {
-        //if match not found then we will again flip it back
-        firstCard.classList.remove("flipped");
-        secondCard.classList.remove("flipped");
+      const firstImg = firstCard.querySelector(".front").src;
+      const secondImg = secondCard.querySelector(".front").src;
+      //matching the urls of both card
+
+      if (firstImg === secondImg) {
+        matchCount++;
+        document.querySelector(".match-count").innerHTML = matchCount;
+        successSound.currentTime = 0;
+        successSound.play();
+        //in case of card match - flip is not done and cards fill remain flipped to its front face
         firstCard = null;
         secondCard = null;
         lockedCard = false;
-      }, 1000);
-    }
+
+        const allCards = document.querySelectorAll(".card");
+        const allflipped = [...allCards].every((card) =>
+          card.classList.contains("flipped"),
+        );
+
+        if (allflipped) {
+          mainGameSound.pause();
+          victorySound.volume = 0.8;
+          victorySound.play();
+          recordGameTime();
+          setTimeout(() => {
+            winningBoard.classList.remove("winningpopup-hidden");
+          }, 300);
+          document.querySelector(".gameScore").innerHTML =
+            "Your Score: " +
+            calculateScore(
+              saveMode || "easy",
+              moveCount,
+              matchCount,
+              minutes,
+              seconds,
+            );
+
+          let newScore = calculateScore(
+            saveMode || "easy",
+            moveCount,
+            matchCount,
+            minutes,
+            seconds,
+          );
+          if (newScore > bestScore) {
+            bestScore = newScore;
+            document.querySelector(".score-count").innerHTML = bestScore;
+          }
+        }
+      } else {
+        setTimeout(() => {
+          //if match not found then we will again flip it back
+          firstCard.classList.remove("flipped");
+          secondCard.classList.remove("flipped");
+          firstCard = null;
+          secondCard = null;
+          lockedCard = false;
+        }, 1000);
+      }
+    });
   });
-});
- 
 }
 
-
 const gameModeOptions = document.querySelector(".mode");
-const modeOptions = document.querySelector(".options")
+const modeOptions = document.querySelector(".options");
 
-gameModeOptions.addEventListener("click",()=>
-{
-  modeOptions.classList.toggle("show")
+gameModeOptions.addEventListener("click", () => {
+  modeOptions.classList.toggle("show");
 });
 
-const innerOptions = document.querySelectorAll(".option")
-let saveMode   = '' ;
-   
-innerOptions.forEach((option)=>
-{
-  option.addEventListener("click",()=>
-  {
-       document.querySelector(".mode-name").innerHTML = option.textContent;
-       saveMode = option.textContent;
-        matchCount = 0 
-        moveCount = 0
-      count = selectedMode(saveMode)
-     document.querySelector(".match-count").innerHTML = 0;
-          document.querySelector(".move-count").innerHTML = 0;
- 
-  })
-})
+const innerOptions = document.querySelectorAll(".option");
+let saveMode = "";
+
+innerOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    document.querySelector(".mode-name").innerHTML = option.textContent;
+    saveMode = option.textContent;
+    matchCount = 0;
+    moveCount = 0;
+    count = selectedMode(saveMode);
+
+    document.querySelector(".match-count").innerHTML = 0;
+    document.querySelector(".move-count").innerHTML = 0;
+  });
+});
 
 //restart game
 
-const restartGame = document.querySelector(".restart")
+const restartGame = document.querySelector(".restart");
 
 restartGame.addEventListener("click", () => {
   document.querySelector(".gameboard").innerHTML = " ";
@@ -239,7 +248,6 @@ restartGame.addEventListener("click", () => {
   document.querySelector(".timing").innerHTML = `00:00`;
 });
 
- 
 //game sound popup
 const soundClass = document.querySelector(".gamesoundpopup");
 const settingBtn = document.querySelector(".setting");
@@ -255,10 +263,9 @@ settingBtn.addEventListener("click", () => {
   soundClass.classList.toggle("hidden");
 });
 
-
-const getbtn = document.querySelector(".main-game-sound-button")
-const buttonSlider = document.querySelector(".button-slider")
-const soundMode = document.querySelector(".main-game-sound-name")
+const getbtn = document.querySelector(".main-game-sound-button");
+const buttonSlider = document.querySelector(".button-slider");
+const soundMode = document.querySelector(".main-game-sound-name");
 
 getbtn.addEventListener("click", () => {
   if (mainGameSound.paused) {
@@ -268,27 +275,26 @@ getbtn.addEventListener("click", () => {
   } else {
     mainGameSound.pause();
     getbtn.querySelector("span").textContent = "OFF";
-  buttonSlider.style.left = "calc(100% - 25px)";
+    buttonSlider.style.left = "calc(100% - 25px)";
   }
-}); 
+});
 
-const startPage = document.querySelector(".instruction")
-const startBtn = document.querySelector(".start-game")
- let playerName = '';
- 
-startBtn.addEventListener("click",()=>
-{
-startPage.classList.add("instruction-hidden")
- playerName = document.querySelector(".playerName input").value;
- document.querySelector(".winningPlayerName").innerHTML = `Congratulations ${playerName}`;
-  callTimer()
-}) 
+const startPage = document.querySelector(".instruction");
+const startBtn = document.querySelector(".start-game");
+let playerName = "";
 
+startBtn.addEventListener("click", () => {
+  startPage.classList.add("instruction-hidden");
+  playerName = document.querySelector(".playerName input").value;
+  document.querySelector(".winningPlayerName").innerHTML =
+    `Congratulations ${playerName}`;
+  callTimer();
+});
 
 //game timer logic
 
 let [seconds, minutes] = [0, 0];
-let timerInterval = null; 
+let timerInterval = null;
 
 function GameTimer() {
   seconds++;
@@ -300,11 +306,11 @@ function GameTimer() {
   let secondsTimer = seconds < 10 ? "0" + seconds : seconds;
   let minuteTimer = minutes < 10 ? "0" + minutes : minutes;
 
-  document.querySelector(".timing").innerHTML = `${minuteTimer}:${secondsTimer}`;
+  document.querySelector(".timing").innerHTML =
+    `${minuteTimer}:${secondsTimer}`;
 }
 
 function callTimer() {
- 
   clearInterval(timerInterval);
   timerInterval = setInterval(GameTimer, 1000);
 }
@@ -319,12 +325,10 @@ const playAgain = document.getElementById("playagainbtn");
 playAgain.addEventListener("click", () => {
   document.querySelector(".winningpopup").classList.add("winningpopup-hidden");
 
- 
   matchCount = 0;
   moveCount = 0;
   document.querySelector(".match-count").innerHTML = matchCount;
   document.querySelector(".move-count").innerHTML = moveCount;
-
 
   seconds = 0;
   minutes = 0;
@@ -336,7 +340,7 @@ playAgain.addEventListener("click", () => {
   document.querySelector(".mode-name").innerHTML = saveMode;
   renderGameBoard(selectedMode(saveMode));
 
- isSoundOn = !isSoundOn;
+  isSoundOn = !isSoundOn;
 
   if (isSoundOn) {
     mainGameSound.play();
@@ -347,13 +351,11 @@ playAgain.addEventListener("click", () => {
     getbtn.querySelector("span").textContent = "OFF";
     buttonSlider.style.left = "calc(100% - 25px)";
   }
-document.querySelector(".score-count").innerHTML = bestScore;
+  document.querySelector(".score-count").innerHTML = bestScore;
 
   callTimer();
-
 });
 
- 
 function calculateScore(mode, moveCount, matchCount, minutes, seconds) {
   const totalSeconds = minutes * 60 + seconds;
 
@@ -396,4 +398,3 @@ function calculateScore(mode, moveCount, matchCount, minutes, seconds) {
   const finalScore = Math.max(0, Math.round(rawScore)); // Never negative
   return finalScore;
 }
-
